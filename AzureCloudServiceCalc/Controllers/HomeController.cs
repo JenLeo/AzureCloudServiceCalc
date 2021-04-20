194724352716cca7e1ca28d2790d449fc0340060
JenLeo <colleranjenny@gmail.com>
2021-04-20 19:59:04 +0100

    #

---
+++ AzureCloudServiceCalc/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using AzureCloudServiceCalc.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureCloudServiceCalc.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Calculate()
+        {
+            ViewBag.InstanceSize = new SelectList(AzureCloudService.InstanceSizeDescriptions);
+            return View(new AzureCloudService() { NoInstances = 2 });
+        }
+
+        [HttpPost]
+        public ActionResult Calculate(AzureCloudService service)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Confirm", service);
+            }
+            else
+            {
+                ViewBag.InstanceSize = new SelectList(AzureCloudService.InstanceSizeDescriptions);
+                return View(service);
+            }
+        }
+        // show confirmation
+        public ActionResult Confirm(AzureCloudService service)
+        {
+            return View(service);
+        }
+    }
+}
---
+++ AzureCloudServiceCalc/Models/AzureCloudService.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureCloudServiceCalc.Models
+{
+    public class AzureCloudService
+        {
+            // instance size descriptions for a cloud service
+            public static String[] InstanceSizeDescriptions
+            {
+                get
+                {
+                    return new String[] { "Very Small", "Small", "Medium", "Large", "Very Large", "A5", "A6" };
+                }
+            }
+
+            // corresponding prices per hour ($) for each instance size as above
+            public static double[] InstanceSizePrices
+            {
+                get
+                {
+                    return new double[] { 0.02, 0.08, 0.16, 0.32, 0.64, 0.90, 1.80 };
+                }
+            }
+        // no of instances for a service
+        [Required(ErrorMessage = "Required field!")]
+        [Range(2, Int32.MaxValue, ErrorMessage = "At least 2 instances required")]
+        [DisplayName("No of Instances")]
+        public int NoInstances { get; set; }
+
+        // size of an instance e.g. very small, small etc
+        [Required(ErrorMessage = "Required field!")]
+        [DisplayName("Instance Size")]
+        public String InstanceSize { get; set; }
+        // get the cost of a service based on #instances and size for a year
+        public double Cost
+        {
+            get
+            {
+                int size = 0;
+                for (int i = 0; i < AzureCloudService.InstanceSizeDescriptions.Length; i++)
+                {
+                    if (AzureCloudService.InstanceSizeDescriptions[i] == this.InstanceSize)
+                    {
+                        size = i;
+                        break;
+                    }
+                }
+                double hourlyPrice = NoInstances * InstanceSizePrices[size];
+                double dailyPrice = hourlyPrice * 24;
+                double yearlyPrice;
+
+                if (DateTime.IsLeapYear(DateTime.Now.Year))
+                {
+                    yearlyPrice = dailyPrice * 366;
+                }
+                else
+                {
+                    yearlyPrice = dailyPrice * 365;
+                }
+                return yearlyPrice;
+            }
+        }
+    }
+}
